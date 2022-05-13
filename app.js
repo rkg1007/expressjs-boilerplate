8fb3d79e0c8ace925ae82421051f4d2cbe71a0d7
rohit <rkg10072000@gmail.com>
2022-05-13 17:11:28 +0530

    implemented the connect database code

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 require("express-async-error");
 const express = require("express");
 
+const connectDb = require("./utils/connect-db");
 const notFound = require("./middlewares/not-found");
 const errorHandler = require("./middlewares/error-handler");
-
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -18,7 +18,12 @@ app.use(notFound);
 app.use(errorHandler);
 
 const start = (port, url) => {
-  app.listen(port, console.log(`server is up and running at port ${port}...`));
+  try {
+    connectDb(url)
+    app.listen(port, console.log(`server is up and running at port ${port}...`));
+  } catch (error) {
+    console.log("Error while connecting to the database :- " + error.message);
+  }
 };
 
 start(PORT, MONGO_URI);
