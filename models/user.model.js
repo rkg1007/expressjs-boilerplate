70dda3928d8e913cf090e4e71b11cf85f6e17f4d
Rohit <rkg10072000@gmail.com>
2022-05-19 17:05:09 +0530

    implemented password hashing and password comparing logic

--- models/user.model.js
+++ models/user.model.js
@@ -1,34 +1,46 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
-const UserSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "please provide name"],
-    minlength: 3,
-    maxlength: 50
+const UserSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "please provide name"],
+      minlength: 3,
+      maxlength: 50,
+    },
+    email: {
+      type: String,
+      required: [true, "please provide email"],
+      validate: {
+        validator: validator.isEmail,
+        message: "please provide valid email",
+      },
+    },
+    password: {
+      type: String,
+      required: [true, "please provide password"],
+      minlength: 3,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
   },
-  email: {
-    type: String,
-    required: [true, "please provide email"],
-    validate: {
-      validator: validator.isEmail,
-      message: "please provide valid email"
-    }
-  },
-  password: {
-    type: String,
-    required: [true, "please provide password"],
-    minlength: 3
-  },
-  role: {
-    type: String,
-    enum: ["admin", "user"],
-    default: "user"
+  {
+    timestamps: true,
   }
-},
-{
-  timestamps: true
+);
+
+UserSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 10);
 });
+
+UserSchema.methods.comparePassword = async function (password) {
+  const isPasswordCorrect = await bcrypt.compare(password, this.password);
+  return isPasswordCorrect;
+};
 
 module.export = mongoose.model("User", UserSchema);
