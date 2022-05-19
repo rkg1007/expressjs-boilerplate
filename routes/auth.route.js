f71a03231957958cd4c5f90ff52c415dd6cd9b58
Rohit <rkg10072000@gmail.com>
2022-05-19 22:25:25 +0530

    refactored the code

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { authController } = require("../controllers");
+const authController = require("../controllers/auth.controller");
 
 const router = express.Router();
 
