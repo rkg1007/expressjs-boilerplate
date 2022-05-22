f08b5892f3fb00ddc9afa7dcb4fa44555cb64805
rohit <rkg10072000@gmail.com>
2022-05-22 20:05:02 +0530

    refactored the logout route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const { route } = require("express/lib/router");
 const authController = require("../controllers/auth.controller");
+const { authenticate } = require("../middlewares/authentication");
 
 const router = express.Router();
 
 router.route("/register").post(authController.register);
 router.route("/verify-email").post(authController.verifyEmail);
 router.route("/login").post(authController.login);
-router.route("/logout").get(authController.logout);
+router.route("/logout").delete(authenticate(), authController.logout);
 
 module.exports = router;
